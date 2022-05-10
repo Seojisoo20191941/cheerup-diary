c816e59c6756f666bb770ee46b10d789e7bc1ec4
Seojisoo20191941 <amy0480@naver.com>
2022-05-11 03:46:15 +0900

    feat:add checking sex

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
+let girl=0;
+let boy=0;
+let sunny=0;
+let cloudy=0;
+let rain=0;
+let snow=0;
+
+function girlCheck(){
+    girl=1;
+}
+function boyCheck(){
+    boy=1;
+}
+function sunnyCheck(){
+    sunny=1;
+}
+function cloudyCheck(){
+    cloudy=1;
+}
+function rainCheck(){
+    rain=1;
+}
+function snowCheck(){
+    snow=1;
+}
 function printName()  {
     let date = document.getElementById('date').value;
     let name = document.getElementById('name').value;
     console.log(name);
     console.log(date);
+    console.log(girl);
 
   }
