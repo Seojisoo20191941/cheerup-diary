b969ca7708c76a090f1987958a6463095707f408
Seojisoo20191941 <amy0480@naver.com>
2022-05-12 03:24:56 +0900

    feat:finished first page

--- main.js
+++ main.js
@@ -6,23 +6,48 @@ let rain=0;
 let snow=0;
 
 function girlCheck(){
+    document.getElementById('girl').style.width="16vh";
     girl=1;
+
 }
 function boyCheck(){
     boy=1;
+    document.getElementById('boy').style.width="16vh";
+
 }
 function sunnyCheck(){
     sunny=1;
+    document.getElementById('sunny').src="img/sunny-check.png";
+
 }
 function cloudyCheck(){
     cloudy=1;
+    document.getElementById('cloudy').src="img/cloud-check.png";
+
 }
 function rainCheck(){
     rain=1;
+    document.getElementById('rain').src="img/rain-uncheck.png";
+
 }
 function snowCheck(){
     snow=1;
+    document.getElementById('snow').src="img/snow-uncheck.png";
+
 }
+function girlOver(){
+    document.getElementById('girl').style.width="16vh";
+}
+function boyOver(){
+    document.getElementById('boy').style.width="16vh";
+}  
+function girlOut(){
+    document.getElementById('girl').style.width="14vh";
+}
+function boyOut(){
+    document.getElementById('boy').style.width="14vh";
+}    
+
 function sunnyOver(){
     document.getElementById('sunny').src="img/sunny-check.png";
 }
@@ -55,4 +80,7 @@ function printName()  {
     console.log(date);    
     document.querySelector(".startpage").style.display="none";
     document.querySelector(".resultpage").style.display="block";
+}
+function replay(){
+    location.reload();
 }
