b37799c5992c0e596bf7db9bf184279c0a524f84
Seojisoo20191941 <amy0480@naver.com>
2022-05-14 17:46:25 +0900

    feat:finished all features

--- main.js
+++ main.js
@@ -40,6 +40,8 @@
 function printName()  {
     let date = document.querySelector('#date').value;
     let myname = document.querySelector('#name').value;
+    // let resultimage = document.querySelector("#resultimage").src;
+    // let comment=document.querySelector("#comment").innerHTML;
     console.log(myname);
     console.log(date);    
     document.querySelector(".startpage").style.display="none";
@@ -57,21 +59,43 @@ function printName()  {
     console.log(girlCheck);
 
     // girl
-    // if (girl>0){
+    if (girlCheck){
+        if (sunnyCheck){
+            document.querySelector("#resultimage").src="img/sunny_girl.png";
+            document.querySelector("#comment").innerHTML="화창한 날씨처럼 행복해!";
+        }
+        else if (cloudyCheck){
+            document.querySelector("#resultimage").src="img/cloudy_girl.png";
+            document.querySelector("#comment").innerHTML="바빠도 하늘은 보고 살자!";
+        }
+        else if (rainCheck){
+            document.querySelector("#resultimage").src="img/rain_girl.png";
+            document.querySelector("#comment").innerHTML="첨벙첨벙 자연asmr 들으며 푹 자!";
+        }
+        else if (snowCheck){
+            document.querySelector("#resultimage").src="img/snow_girl.png";
+            document.querySelector("#comment").innerHTML="함박눈이 내리는 날, 함박웃음을 지어보자!";
+        }
+    }
 
-    // }
-    // sunny
-    // cloudy
-    // rain
-    // snow
-    // if (boy>0){
-
-    // }
-    // boy
-    // sunny
-    // cloudy
-    // rain
-    // snow
+    if (boyCheck){
+        if (sunnyCheck){
+            document.querySelector("#resultimage").src="img/sunny_boy.png";
+            document.querySelector("#comment").innerHTML="화창한 날씨처럼 행복해!";
+        }
+        else if (cloudyCheck){
+            document.querySelector("#resultimage").src="img/cloudy_boy.png";
+            document.querySelector("#comment").innerHTML="바빠도 하늘은 보고 살자!";
+        }
+        else if (rainCheck){
+            document.querySelector("#resultimage").src="img/rain_boy.png";
+            document.querySelector("#comment").innerHTML="첨벙첨벙 자연asmr 들으며 푹 자!";
+        }
+        else if (snowCheck){
+            document.querySelector("#resultimage").src="img/snow_boy.png";
+            document.querySelector("#comment").innerHTML="함박눈이 내리는 날, 함박웃음을 지어보자!";
+        }
+    }
 
 }
 function replay(){
