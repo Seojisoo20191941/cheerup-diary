235ceeb546fc93fd2daaf12a02707433d6b3f488
Seojisoo20191941 <amy0480@naver.com>
2022-05-18 01:10:26 +0900

    fix:fixed js

--- main.js
+++ main.js
@@ -132,20 +132,3 @@ $(function(){
     }
 });
 
-
-// //이미지(png)로 다운로드
-// function PrintDiv(div){
-// 	div = div[0]
-// 	html2canvas(div).then(function(canvas){
-// 		var myImage = canvas.toDataURL();
-// 		downloadURI(myImage, "저장이미지이름.png") 
-// 	});
-// }
-
-// function downloadURI(uri, name){
-// 	var link = document.createElement("a")
-// 	link.download = name;
-// 	link.href = uri;
-// 	document.body.appendChild(link);
-// 	link.click();
-// }
